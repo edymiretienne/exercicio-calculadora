2b73a9eed4200b818178c35f51ae0749c5f530aa
edymiretienne <eebls@cin.ufpe.br>
2018-09-05 00:19:13 -0300

    - add listeners, toast and saved instance - Change textview type

--- calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
@@ -2,12 +2,51 @@ package br.ufpe.cin.if710.calculadora
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
 
 class MainActivity : Activity() {
+
+    private var results = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text_info.text = savedInstanceState?.getString(results)
+
+        btn_0.setOnClickListener { text_calc.append(btn_0.text) }
+        btn_1.setOnClickListener { text_calc.append(btn_1.text) }
+        btn_2.setOnClickListener { text_calc.append(btn_2.text) }
+        btn_3.setOnClickListener { text_calc.append(btn_3.text) }
+        btn_4.setOnClickListener { text_calc.append(btn_4.text) }
+        btn_5.setOnClickListener { text_calc.append(btn_5.text) }
+        btn_6.setOnClickListener { text_calc.append(btn_6.text) }
+        btn_7.setOnClickListener { text_calc.append(btn_7.text) }
+        btn_8.setOnClickListener { text_calc.append(btn_8.text) }
+        btn_9.setOnClickListener { text_calc.append(btn_9.text) }
+        btn_Add.setOnClickListener { text_calc.append(btn_Add.text) }
+        btn_Divide.setOnClickListener { text_calc.append(btn_Divide.text) }
+        btn_Multiply.setOnClickListener { text_calc.append(btn_Multiply.text) }
+        btn_Subtract.setOnClickListener { text_calc.append(btn_Subtract.text) }
+        btn_Dot.setOnClickListener { text_calc.append(btn_Dot.text) }
+        btn_LParen.setOnClickListener { text_calc.append(btn_LParen.text) }
+        btn_RParen.setOnClickListener { text_calc.append(btn_RParen.text) }
+        btn_Power.setOnClickListener { text_calc.append(btn_Power.text) }
+        btn_Clear.setOnClickListener{ text_calc.text.clear() }
+        btn_Equal.setOnClickListener {
+            try {
+                text_info.text = eval(text_calc.text.toString()).toString()
+            } catch (e: RuntimeException) {
+                Toast.makeText(this, "Expressão Inválida! Preste Atenção, rapaz!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putString(results,text_info.text.toString())
+        super.onSaveInstanceState(outState)
     }
 
     //Como usar a função:
